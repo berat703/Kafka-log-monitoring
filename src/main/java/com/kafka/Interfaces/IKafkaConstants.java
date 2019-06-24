f721343b8776f58bc4e53af1429114fe580b806c
Berat <beratnakip@gmail.com>
2019-06-24 15:29:04 +0300

    changed kafka connections

--- src/main/java/com/kafka/Interfaces/IKafkaConstants.java
+++ src/main/java/com/kafka/Interfaces/IKafkaConstants.java
@@ -1,8 +1,8 @@
 package com.kafka.Interfaces;
 
 public interface IKafkaConstants {
-    String KAFKA_BROKERS = System.getenv().get("KAFKA_BROKER");
-    Integer MESSAGE_COUNT=1000;
+    String KAFKA_BROKERS = "localhost:9092"; // eski halini docker composedan kafka config ayarlarını yapmak için kullanmıştım
+    Integer MESSAGE_COUNT = 1000;             // suanda localhost 9092 portundan kafka servera baglayacagız
     String CLIENT_ID="client1";
     String TOPIC_NAME="log-monitoring";
     String GROUP_ID_CONFIG="consumerGroup1";
